6ab6753de58ca29727e19a16ed5939eced3c8403
Jony <jamsi123@bk.ru>
2020-06-08 18:17:24 +0300

    finish django project

--- store/mainapp/views.py
+++ store/mainapp/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
+from django.template.loader import render_to_string
 
 from mainapp.models import Product
 from mainapp.forms import CreateProductForm
@@ -17,11 +18,12 @@ class ProductList(View):
         return render(request, 'mainapp/main_base.html', context=content)
 
     def post(self, request):
-        form = CreateProductForm(request.POST)
+        form = CreateProductForm(request.POST, request.FILES)
         
         if form.is_valid():
-            new_product = form.save()
-            # return redirect('main:index')
-            return JsonResponse({'product': model_to_dict(new_product)})
+            form.save()
+
+            result = render_to_string('mainapp/includes/inc__product_list.html', context={'products': Product.objects.all()})
+            return JsonResponse({'result': result})
         else:
             return redirect('main:index')
--- store/static/js/product_add.js
+++ store/static/js/product_add.js
@@ -1,41 +1,31 @@
+`use strict`
+
 $(document).ready(function(){
+    if('function' === typeof importScripts) {
+        importScripts('popups.js');
+    }
 
     $("#createProductButton").click(function(evt){
-        // let serializedData = $("#ceateProductForm").serialize();
         evt.preventDefault();
         let form = $("#ceateProductForm")[0];
 
         $.ajax({
             url: $("#ceateProductForm").data('url'),
-            // data: serializedData,
-            type: 'post',
+            type: 'POST',
             data: new FormData(form),
-            // cache: false,
             contentType: false,
             processData: false,
             success: function(response){
-                $(".content__row").append('<div class="content__column"><div class="content__item"><div class="content__img"><img src="/media/' + 
-                response.product.image + '" alt=""></div><div class="subtitle">' + 
-                response.product.name + '</div><div class="price">' + response.product.price + '</div><div class="description"><p>' + 
-                response.product.desc + '</p></div><a href="#" class="add__btn">Buy</a></div></div>')
+                if (!response.result){
+                    console.log('no')
+                }else{
+                    $(".content__row").html(response.result);
+                    const popupActive = document.querySelector('.popup.open');
+                    popupClose
+                    popupClose(popupActive);
+                }
             }
         })
-        
-
-        // $.ajax({
-        //     url: $("#ceateProductForm").data('url'),
-        //     data: serializedData,
-        //     type: 'post',
-        //     success: function(response){
-        //         $(".content__row").append('<div class="content__column"><div class="content__item"><div class="content__img"><img src="/media/' + 
-        //         response.product.image + '" alt=""></div><div class="subtitle">' + 
-        //         response.product.name + '</div><div class="price">' + response.product.price + '</div><div class="description"><p>' + 
-        //         response.product.desc + '</p></div><a href="#" class="add__btn">Buy</a></div></div>')
-        //     }
-        // })
-
     });
-
-
-
+    
 });
